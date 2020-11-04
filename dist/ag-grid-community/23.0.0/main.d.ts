dd83019870e8bfa01fa347e994386e07d96c244e
jmfrancois <jmfrancois@talend.com>
2020-11-04 10:38:19 +0100

    fix: cleanup, remove useless files

--- dist/ag-grid-community/23.0.0/main.d.ts
+++
@@ -1,5 +0,0 @@
-// Type definitions for ag-grid-community v23.0.0
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ag-grid/>
-export * from "@ag-grid-community/csv-export";
-export * from "./dist/lib/main";
